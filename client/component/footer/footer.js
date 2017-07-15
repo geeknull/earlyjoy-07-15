563de2174bf0e904f59a827ef95df32626fd8388
geeknull <geeknull@gmail.com>
2017-07-15 12:11:32 +0800

    完成底部导航

--- client/component/footer/footer.js
+++ client/component/footer/footer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 export default class extends Component {
   constructor() {
@@ -12,7 +13,22 @@ export default class extends Component {
   render() {
     return (
       <footer className="footer">
-        <span>我是底部信息</span>
+        <Link className="footer-item" to="/index">
+          <i className="iconfont icon-zhuye"/>
+          <span>首页</span>
+        </Link>
+        <Link className="footer-item" to="/today">
+          <i className="iconfont icon-liebiao"/>
+          <span>今天</span>
+        </Link>
+        <Link className="footer-item" to="/rank">
+          <i className="iconfont icon-paixingbang"/>
+          <span>排行</span>
+        </Link>
+        <Link className="footer-item" to="/mine">
+          <i className="iconfont icon-gerenzhongxin"/>
+          <span>我的</span>
+        </Link>
       </footer>
     )
   }
